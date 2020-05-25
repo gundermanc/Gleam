9a55b3403c3423bdfa1ca1ec85d02cf6375e5741
Christian Gunderman <gundermanc@gmail.com>
2020-05-25 13:34:28 -0700

    Hacked together initial text rendering.

--- Renderer/App/RemoteView.cpp
+++ Renderer/App/RemoteView.cpp
@@ -8,7 +8,14 @@ namespace
 {
     nlohmann::json ReadJsonResponse(AbstractLogger& logger, ChildProcess& childProcess)
     {
-        const std::string line = childProcess.ReadStdOutLine();
+        std::string line;
+
+        // TODO: what's going on here? We seem to frequently get back empty lines.
+        while (line.size() == 0 ||
+            line.size() == 1 && line[0] == '\n')
+        {
+            line = childProcess.ReadStdOutLine();
+        }
 
         try
         {
@@ -17,6 +24,7 @@ namespace
         catch (const std::exception& ex)
         {
             logger.Log(LogSeverity::Warning, "Failed to deserialize JSON response " + std::string(ex.what()));
+            return nlohmann::json();
         }
     }
 
@@ -36,22 +44,91 @@ namespace
         }
     }
 
-    bool TryGetString(
-        nlohmann::json json,
+    bool TryGetUint(
+        nlohmann::json& json,
         const std::string& key,
-        std::string& value)
+        unsigned int& value)
     {
-        if (json != nullptr)
+        auto valueJson = json[key];
+        if (valueJson != nullptr && valueJson.is_number_integer())
         {
-            auto valueJson = json[key];
-            if (valueJson != nullptr && valueJson.is_string())
-            {
-                value = valueJson;
-                return true;
-            }
+            value = valueJson;
+            return true;
         }
 
         return false;
+    }
+
+    bool TryGetString(
+        nlohmann::json& json,
+        const std::string& key,
+        std::string& value)
+    {
+        auto valueJson = json[key];
+        if (valueJson != nullptr && valueJson.is_string())
+        {
+            value = valueJson;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool TryGetObjectJson(
+        nlohmann::json& json,
+        const std::string& key,
+        nlohmann::json& objJson)
+    {
+        auto valueJson = json[key];
+        if (valueJson != nullptr && valueJson.is_object())
+        {
+            objJson = valueJson;
+            return true;
+        }
+
+        return false;
+    }
+
+    void RenderInstructions(
+        AbstractLogger& logger,
+        std::shared_ptr<AbstractGraphicsContext> graphicsContext,
+        nlohmann::json& responseJson)
+    {
+        // Validate response message.
+        std::string name;
+        if (!TryGetString(responseJson, "name", name) || name != "gleam/render/doLayout")
+        {
+            logger.LogAndFailFast("RenderInstructions called without proper JSON response body.");
+        }
+
+        auto paramsJson = responseJson["params"];
+        auto instructionsJson = paramsJson["instructions"];
+        if (instructionsJson.is_array())
+        {
+            for (auto it = instructionsJson.begin(); it != instructionsJson.end(); ++it)
+            {
+                unsigned int type = 0;
+                unsigned int x = 0;
+                unsigned int y = 0;
+                unsigned int width = 0;
+                unsigned int height = 0;
+                nlohmann::json instructionParamsJson;
+                std::string text;
+                unsigned int size = 0;
+
+                if (TryGetUint(*it, "type", type) &&
+                    TryGetUint(*it, "x", x) &&
+                    TryGetUint(*it, "y", y) &&
+                    TryGetUint(*it, "width", width) &&
+                    TryGetUint(*it, "height", height) &&
+                    TryGetObjectJson(*it, "params", instructionParamsJson) &&
+                    TryGetString(instructionParamsJson, "text", text) &&
+                    TryGetUint(instructionParamsJson, "size", size))
+                {
+                    graphicsContext->DrawText(text, colors::Green, size, x, y);
+                }
+            }
+        }
     }
 }
 
@@ -72,10 +149,12 @@ RemoteView::RemoteView(
     // Check for an 'initialized' message.
     auto response = ReadJsonResponse(this->logger, *this->serverProcess);
     std::string name;
-    if (!TryGetString(response, "name", name) || name != "gleam/editor/initialize")
+    if (!TryGetString(response, "name", name) || name != "gleam/initialize")
     {
         this->logger.LogAndFailFast("Failed to initialize server. Unrecognized response.");
     }
+
+    this->SendDimensions();
 }
 
 RemoteView::~RemoteView()
@@ -89,11 +168,56 @@ void RemoteView::Render(std::shared_ptr<AbstractGraphicsContext> graphicsContext
     nlohmann::json requestJson;
     requestJson["name"] = std::string("gleam/render/layout");
     requestJson["logString"] = std::string("Request render layout");
+
+    nlohmann::json paramsJson;
+    paramsJson["x"] = this->GetX().GetValue();
+    paramsJson["y"] = this->GetY().GetValue();
+    paramsJson["width"] = this->GetWidth().GetValue();
+    paramsJson["height"] = this->GetHeight().GetValue();
+
+    requestJson["params"] = paramsJson;
+
     WriteJsonRequest(this->logger, *this->serverProcess, requestJson);
     auto response = ReadJsonResponse(this->logger, *this->serverProcess);
 
+    // Validate response message.
+    std::string name;
+    if (!TryGetString(response, "name", name) ||
+        (name != "gleam/ack" && name != "gleam/render/doLayout"))
+    {
+        this->logger.LogAndFailFast("Unrecognized response to gleam/render/layout: " + name);
+    }
+
     // Call base renderer.
     AbstractControl::Render(graphicsContext);
+
+    // If drawing was requested, go ahead and do it.
+    if (name == "gleam/render/doLayout")
+    {
+        RenderInstructions(this->logger, graphicsContext, response);
+    }
+}
+
+// Sends a command to the compute process with updated screen dimensions.
+void RemoteView::SendDimensions()
+{
+    nlohmann::json requestJson;
+    requestJson["name"] = std::string("gleam/render/dimensions");
+    requestJson["logString"] = std::string("Sending updated dimensions");
+
+    nlohmann::json paramsJson;
+    paramsJson["width"] = this->GetWidth().GetValue();
+    paramsJson["height"] = this->GetHeight().GetValue();
+
+    requestJson["params"] = paramsJson;
+    WriteJsonRequest(this->logger, *this->serverProcess, requestJson);
+    auto response = ReadJsonResponse(this->logger, *this->serverProcess);
+
+    std::string name;
+    if (!TryGetString(response, "name", name) || name != "gleam/ack")
+    {
+        this->logger.LogAndFailFast("Failed to update server render dimensions. Unrecognized response.");
+    }
 }
 
 void ShowRemoteView(AbstractLogger& logger, AbstractViewport& viewport)
--- Renderer/App/RemoteView.h
+++ Renderer/App/RemoteView.h
@@ -27,4 +27,6 @@ public:
 private:
     AbstractLogger& logger;
     ChildProcess* serverProcess;
+
+    void SendDimensions();
 };
